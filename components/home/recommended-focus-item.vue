3baaf47c6a303c1b9d286d67fd37a27660966c95
DESKTOP-P635RSB\Think <zhangyunfeng0101@gmail.com>
2019-01-27 19:55:29 +0800

    完成首页“

---
+++ components/home/recommended-focus-item.vue
@@ -0,0 +1,61 @@
+<template>
+  <navigator class="suggestion-navigator" :url="link">
+    <image class="suggestion-bg" :src="img"></image>
+    <text class="suggestion-item-title">{{title}}</text>
+    <view class="bottom">
+      <text class="views">{{views}}</text>
+      <text class="shares">{{shares}}</text>
+    </view>
+  </navigator>  
+</template>
+
+<script>
+  export default {
+    name: "RecommendedFocusItem",
+    props: {
+      link: String,
+      img: String,
+      title: String,
+      views: Number,
+      shares: Number
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .suggestion-navigator {
+    width: 277.778upx;
+    height: auto;
+    
+    .suggestion-bg {
+      width: 100%;
+      height: 208.333upx;
+    }
+
+    .suggestion-item-title {
+      width: 240.278upx;
+      height: 27.778upx;
+      font-size: 27.778upx;
+      font-weight:400;
+      color:rgba(51,51,51,1);
+      line-height: 38.889upx;
+    }
+
+    .bottom {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: center;
+
+      width: 100%;
+      height: 41.667upx;
+
+      font-size:32px;
+      font-weight:300;
+      color:rgba(153,153,153,1);
+      line-height: 41.667upx;
+    }
+
+  }
+      
+</style>
--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,67 +1,456 @@
 <template>
-  <view class="content">
-    <view class="top-panel">
-			<view class="search-bar">
+  <scroll-view scroll-y="true" class="scroll-view">
+    <view class="content">
+      <!-- 顶部的大背景 -->
+      <view class="top-panel">
+        <image class="top-panel-bg" src="/static/img/home_header_bg.jpg"/>>
+        <view class="buttons">
+          <view class="button checkout">
+            <image src="/static/icon/home/icon_checkout.png"/>
+            <text>签到</text>
+          </view>
+          <view class="button view-points">
+            <image src="/static/icon/home/icon_pig.png"/>
+            <text>查看</text>
+          </view>
+        </view>
       </view>
-		</view>
-  </view>
+
+      <!-- 滑动banner -->
+      <view class="swiper-banners">
+
+        <swiper class="swiper" 
+        :indicator-dots="swiper.indicatorDots"
+        :autoplay="swiper.autoplay"
+        :interval="swiper.interval"
+        :duration="swiper.duration">
+          <swiper-item class="swiper-item" v-for="(image, index) in imgList">
+            <navigator class="banner" url="image.item1.link">
+              <image :src="image.item1.img" class="banner-img"></image>
+            </navigator>
+            <navigator class="banner" url="image.item2.link">
+              <image :src="image.item2.img" class="banner-img"></image>
+            </navigator>
+          </swiper-item>
+        </swiper>
+
+      </view>
+
+      <!-- 焦点推荐 -->
+      <view class="the-focus-sugguestions">
+        <view class="header">
+          <text class="title">焦点推荐</text>
+          <navigator url="#"><text>更多&gt;&gt;</text></navigator>
+        </view>
+        <view class="body">
+          <swiper>
+            <swiper-item class="suggestion-swiper-item" v-for="item in suggestionList">
+              <recommended-focus-item :link="item.link" :img="item.img" :title="item.title" :views="item.views" :shares="item.shares"/>
+            </swiper-item>
+          </swiper>
+        </view>
+      </view>
+
+      <!-- 焦点咨询列表 -->
+      <view class="the-focus-list">
+        <view class="menu-navigator">
+          <swiper class="menu-swiper">
+            <!-- 定义菜单 -->
+            <swiper-item v-for="(menu, index) in menus" class="swiper-menu-item">
+              <view class="menu" :class="index === 0 ? 'activate' : ''">
+                <navigator class="menu" :url="menu.url">
+                  <text>{{ menu.name }}</text>
+                </navigator>
+              </view>
+            </swiper-item>
+
+          </swiper>
+        </view>
+        <view style="width: 100%;height: 1px;background: rgba(229,229,229,1);"></view>
+        <view class="news-list">
+          <view class="news-item" v-for="(news, index) in newsList">
+            <image class="cover" :src="news.cover"></image>
+            <view class="content">
+              <text class="title">{{news.title}}</text>
+              <text class="publish-date">发布时间：{{news.publish_time}}</text>
+              <view class="statistics-info">
+                <view class="statistics-item visits">
+                  <image class="icon-visits"></image>
+                  <text>{{news.visits}}</text>
+                </view>
+                <view class="statistics-item likes">
+                  <image class="icon-likes"></image>
+                  <text>{{news.likes}}</text>
+                </view>
+                <view class="statistics-item shares">
+                  <image class="icon-shares"></image>
+                  <text>{{news.shares}}</text>
+                </view>
+              </view>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+  </scroll-view>
+
 </template>
 
 <script>
 // import { mapState } from "vuex";
 
+import uniNavBar from "@/components/uni-nav-bar.vue";
+
+import uniIcon from "@/components/uni-icon.vue";
+
+import RecommendedFocusItem from "@/components/home/recommended-focus-item.vue";
+
 export default {
-  computed: {
-   
+  components: {
+    uniNavBar,
+    uniIcon,
+    RecommendedFocusItem
   },
-  onShow() {
-    
+
+  data() {
+		return {
+			swiper: {
+			  indicatorDots: true,
+			  autoplay: true,
+			  interval: 2000,
+			  duration: 500
+			},
+			imgList: [
+			  { 
+			    item1: {
+			      img: "/static/img/home/pic_1.png", 
+			      link: "#"
+			    }, 
+			    item2: {
+			      img: "/static/img/home/pic_2.png",
+			      link: "#"
+			    } 
+			  }
+      ],
+      suggestionList: [
+        {title: "测试标题", link: "#", img: "/static/img/home/pic_1.png", views: 0, shares: 0},
+        {title: "测试标题2", link: "#", img: "/static/img/home/pic_2.png", views: 0, shares: 0}
+      ],
+      menus: [
+        {name: "焦点咨询", url: "#"},
+        {name: "社会", url: "#"},
+        {name: "国际", url: "#"},
+        {name: "娱乐", url: "#"},
+        {name: "体育", url: "#"},
+        {name: "科学", url: "#"}
+      ],
+      newsList: [
+        {title: "世界记忆大师教你快速提升记忆力", cover: "/static/img/home/pic_1.png", publish_time: "2019-01-01", visits: 0, likes: 0, shares: 0},
+        {title: "世界记忆大师教你快速提升记忆力", cover: "/static/img/home/pic_1.png", publish_time: "2019-01-01", visits: 0, likes: 0, shares: 0},
+        
+      ]
+		}
   },
-  onLoad() {
-   
-  },
-  methods:{
-  }
+  computed: {},
+
+  onShow() {},
+
+  onLoad() {},
+
+  methods: {}
 };
 </script>
 
 <style lang="less">
-	.content {
-		position: absolute;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-    background-color: #fff;
-    
-    .top-panel {
+.scroll-view {
+  width: 100%;
+  height: 100%;
+
+}
+.content {
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  display: block;
+  // flex-flow: row nowrap;
+  // justify-content: center;
+  text-align: center;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: #F2F2F2;
+
+  .top-panel {
+    position: relative;
+    left: 0;
+    top: 0;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: top;
+    align-items: center;
+
+    width: 750upx;
+    height: 480upx;
+    background-position: center;
+
+    background-size: 100% 100%;
+
+    .top-panel-bg {
       position: absolute;
       left: 0;
       top: 0;
-      
-      display: flex;
-      flex-flow: column nowrap;
-      justify-content: top;
-      align-items: center;
       width: 100%;
-      height: 555.56upx; 
-      background: url('/static/img/home_header_bg.jpg');
-      background-position: center;
-      background-size: 100% 100%;
+    }
 
-      .search-bar {
-        
+    .buttons {
+      position: absolute;
+      left: 0;
+      bottom: 66.667upx;
+      width: 100%;
+      height: 56.94444upx;
+      line-height: 56.944upx;
+
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+
+      color: #fff;
+      font-size: 25upx;
+
+      .button {
+        width:150upx;
+        height:56.944upx;
+        background:rgba(0,0,0,1);
+        opacity:0.2;
+        border-radius:18.889upx;
+
+        color: inherit;
+        font-size: inherit;
         display: flex;
-        width: 548.61upx;
-        height: 49.30upx;
-        margin-top: 70upx;
-        border: 1upx solid #fff;
-        border-radius: 24.65upx;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
 
-        background: #fff;
+        image {
+          width: 40.278upx;
+          height: 34.722upx;
+          line-height: 1;
+          margin-right: 10upx;
+          
+        }
       }
     }
-	}
-	
-	
+
+  }
+
+  .swiper-banners {
+    position: relative;
+    width: 694.444upx;
+    height: 309.722upx;
+    margin: 0 auto;
+    margin-top: -30px;
+
+    background: #fff;
+    border-radius: 41.667upx 41.667upx 0px 0px;
+
+    .swiper-item {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: center;
+
+      .banner {
+        width: 336.806upx;
+        height: 208.333upx;
+
+        .banner-img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
+
+  .the-focus-sugguestions {
+    display: block;
+    width: 100%;
+    height: auto;
+    box-sizing: border-box;
+    margin-top: 10.417upx;
+    background-color: #fff;
+
+    .header {
+      display: flex;
+      width: 100%;
+      height: 69.444upx;
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0 27.778upx;
+
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: center;
+      
+      font-size: 33.333upx;
+      font-weight: 400;
+      color: rgba(51,51,51,1);
+      line-height: 25upx;
+
+      .title {
+        border-left: 8.333upx solid #c73441;
+        padding-left: 12.5upx;
+        font-size: inherit;
+        color: inherit;
+        line-height: inherit;
+        font-weight: inherit;
+      }
+
+
+    }
+
+    .body {
+      width: 100%;
+      height: auto;
+      .suggestion-swiper-item {
+        width: 305.556upx !important;
+        height: auto;
+        line-height: 1;
+        // margin-left: 27.778upx;
+        padding-left: 27.778upx;
+      }
+    }
+  }
+
+  .the-focus-list {
+    margin-top: 10.417upx !important;
+    background-color: #fff;
+
+    .menu-navigator {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 97.917upx;
+      line-height: 97.917upx;
+      box-sizing: border-box;
+
+      .menu-swiper {
+        width: 100%;
+        height: 100%;
+        line-height: inherit;
+
+        .swiper-menu-item {
+          width: 130upx !important;
+          height: 100%;
+          line-height: inherit;
+          box-sizing: border-box;
+
+          .menu {
+            line-height: inherit;
+
+            width: auto !important;
+            height: 100%;
+            font-size: 29.167upx;
+            color: #666;
+            box-sizing: border-box;
+
+            &.activate {
+              font-size: 26.389upx;
+              font-weight: bold;
+              color: #333;
+              border-bottom: 6.944upx solid #E03C45;
+            }
+          }
+        }
+      }
+    }
+
+    .news-list {
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: center;
+      align-items: center;
+
+      .news-item {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        align-items: center;
+
+        width: 694.444upx;
+        height: 267.361upx;
+        background: #fff;
+        box-sizing: border-box;
+        border-bottom: 0.694upx solid #e5e5e5;
+
+        .cover {
+          width: 277.778upx;
+          height: 208.333upx;
+        }
+
+        .content {
+          position: relative;
+          width: 388.889upx;
+          height: 208.333upx;
+          overflow: hidden;
+          text-align: left;
+
+          .title {
+            font-size: 27.778upx;
+            font-family: MicrosoftYaHei;
+            font-weight: 400;
+            color: #333;
+            line-height: 38.889upx;
+          }
+
+          .publish-date {
+            display: block;
+            font-size: 22upx;
+            font-family:MicrosoftYaHeiLight;
+            font-weight:300;
+            color: #666;
+            line-height: 41.667upx;
+          }
+
+          .statistics-info {
+            width: 100%;
+            height: 34.722upx;
+            overflow: hidden;
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: space-between;
+            align-items: center;
+
+            .statistics-item {
+              width: auto;
+              height: 100%;
+
+              &.visits {
+                .icon-visits {
+                  width: 51px;
+                  height: 32px;
+                }
+              }
+              &.likes {
+                .icon-likes {
+                  width: 38px;
+                  height: 38px;
+                }
+
+              }
+              &.shares {
+                width: 41px;
+                height: 44px;
+              }
+            }
+          }
+          
+        }
+      }
+    }
+  }
+}
 </style>
